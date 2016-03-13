502be168d7710cff45251088319d38f4b05b94da
Sagacity07 <ChrisUtter07@gmail.com>
2016-03-13 11:56:04 -0400

    generated folder

---
+++ generated/template/underscore.js
@@ -0,0 +1,13 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["app/templates/hello.us"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="hello">\r\n  ' +
+((__t = ( text )) == null ? '' : __t) +
+'\r\n</div>';
+
+}
+return __p
+};
